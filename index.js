42452dbc7804499a1d7ab9c583f20e986abe1981
Murod <yurayur1990@gmail.com>
2023-03-15 03:52:48 +0500

    installing handlbars and configure it

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import { engine, create } from 'express-handlebars';
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-
 const app = express()
 
+const hbs = create({defaultLayout: 'main', extname: 'hbs' })
+
+app.engine('hbs', hbs.engine); /* Configure the handlebars into hbs */
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
 app.get('/', (req, res)=>{
-  res.sendFile(path.join(__dirname, 'views', 'index.html'))
+  /* res.sendFile(path.join(__dirname, 'views', 'index.html'))  -- this is a rendering method to render the html*/
+  res.render('index');
 })
 
 app.get('/about', (req, res)=>{
-  res.sendFile(path.join(__dirname, 'views', 'about.html'))
+  /* res.sendFile(path.join(__dirname, 'views', 'about.html')) */
+  res.render('about')
 })
 
 const PORT = process.env.PORT || 4100
